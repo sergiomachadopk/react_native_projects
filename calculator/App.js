9e6061e009b2736d9ccf4d7b7782c16b35ed63ef
sergiomachadopk <sergio.machadopk@gmail.com>
2019-06-09 00:38:05 -0300

    arquivos adicionados

--- calculator/App.js
+++ calculator/App.js
@@ -1,29 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
+import Button from './src/components/Button'
+import Display from './src/components/Display'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const initialState = { 
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
+  
+  state = {...initialState}
+
+  addDigit = n => {
+
+    //point validation
+    if ( n === '.' && this.state.displayValue.includes('.')){
+      return
+    }
+
+    const clearDisplay = this.state.displayValue == '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay:false})
+
+    if ( n !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.setState.current] = newValue
+      this.setState({values})
+    }
+
+  }
+  clearMemory = () => {
+    this.setState({...initialState})
+  }
+  setOperation = operation => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Display value={this.state.displayValue}/>
+        <View style={styles.buttons}>
+          <Button  label='AC' triple onClick={this.clearMemory}/>
+          <Button  label='/' operation onClick={() => this.setOperation('/')}/>
+          <Button  label='7' onClick={() => this.addDigit('7')}/>
+          <Button  label='8' onClick={() => this.addDigit('8')}/>
+          <Button  label='9' onClick={() => this.addDigit('9')}/>
+          <Button  label='*' operation onClick={() => this.setOperation('*')}/>
+          <Button  label='4' onClick={() => this.addDigit('4')}/>
+          <Button  label='5' onClick={() => this.addDigit('5')}/>
+          <Button  label='6' onClick={() => this.addDigit('6')}/>
+          <Button  label='-' operation onClick={() => this.setOperation('-')}/>
+          <Button  label='1' onClick={() => this.addDigit('1')}/>
+          <Button  label='2' onClick={() => this.addDigit('2')}/>
+          <Button  label='3' onClick={() => this.addDigit('3')}/>
+          <Button  label='+' operation onClick={() => this.setOperation('+')}/>
+          <Button  label='0' double onClick={() => this.addDigit('0')}/>
+          <Button  label='.' onClick={() => this.addDigit('.')}/>
+          <Button  label='=' operation onClick={() => this.setOperation('=')}/>
+        </View>
       </View>
     );
   }
@@ -32,18 +74,12 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+
+  
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+})
